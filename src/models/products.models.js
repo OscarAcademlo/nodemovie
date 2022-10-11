97adcfece60f7341e26d0377f7640803ce0542b5
OscarAcademlo <oscarns@gmail.com>
2022-10-11 18:08:56 -0300

    first commit

---
+++ src/models/products.models.js
@@ -0,0 +1,28 @@
+const {DataTytpes, DataTypes} = require('sequelize')
+const db = require('../utils/database')
+
+const Products = db.define('products',{
+    id: {
+        primaryKey: true,
+        type: DataTypes.UUID,
+        allowNull: false
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    category: {
+        type: DataTypes.STRING,
+        allowNull:false
+    },
+    price:{
+        type: DataTypes.DOUBLE,
+        allowNull:false
+    },
+    isAvailable:{
+        type: DataTypes.BOOLEAN,
+        allowNull:false
+    }
+})
+
+module.exports = Products
